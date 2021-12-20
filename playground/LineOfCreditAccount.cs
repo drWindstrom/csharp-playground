f666e46ff6b6fd36b3c985d2e18b6afad881e47d
fred <fred@FRED-LAPTOP>
2021-12-20 22:29:38 +0100

    Add project files.

---
+++ playground/LineOfCreditAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playground;
+
+internal class LineOfCreditAccount : BankAccount
+{
+    public override void PerformMonthEndTransactions()
+    {
+        if (Balance < 0)
+        {
+            var interest = -Balance * 0.07m;
+            MakeWithdrawal(interest, DateTime.Now, "Charge monthly interest");
+        }
+    }
+}
+
